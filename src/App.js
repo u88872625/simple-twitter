fd3326a97949a9ab4f228089be443396424d3feb
u88872625 <u88872625@gmail.com>
2023-08-21 08:20:01 +0800

    fix:delete content

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import "./styles/App.scss";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import AdminMainPage from "./pages/AdminMainPage";
-import AdminUserPage from "./pages/AdminUsersPage";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SettingPage from "./pages/SettingPage";
-import SignUpPage from "./pages/SignUpPage";
-import StatusPage from "./pages/StatusPage";
-import UserPage from "./pages/UserPage";
-
-const basename = process.env.PUBLIC_URL;
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
