e117661767d535c43684068c08dbf0dc79a0cfe8
u88872625 <u88872625@gmail.com>
2023-08-23 20:47:03 +0800

    feat:add handleClick

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./LoginPage.module.scss";
 import IconLogo from "../assets/icons/logo.svg";
 import AuthInput from "../components/AuthInput/AuthInput";
 import AuthBtn from "../components/shared/shareBtn/AuthBtn";
-import { Link } from "react-router-dom";
 import clsx from "clsx";
 import Swal from "sweetalert2";
+import { login } from "../api/auth";
 
 const LoginPage = () => {
   const [account, setAccount] = useState("");
@@ -13,41 +13,40 @@ const LoginPage = () => {
   // 後端錯誤訊息判定
   const [errorMessage, setErrorMessage] = useState("");
 
-  // const handleClick = async () => {
-  //   // 若帳號或密碼為空，則返回
-  //   if (account.trim().length === 0) {
-  //     return;
-  //   }
-  //   if (password.trim().length === 0) {
-  //     return;
-  //   }
-  //
-  // const data = await userLogin({
-  //   account,
-  //   password,
-  // });
-  // const token = data.data.token;
-  //
-  // if(data.status==='success'){
-  //   localStorage.setItem("authToken", token);
+  const handleClick = async () => {
+    // 若帳號或密碼為空，則返回
+    if (account.trim().length === 0) {
+      return;
+    }
+    if (password.trim().length === 0) {
+      return;
+    }
 
-  // 登入成功訊息
-  //   Swal.fire({
-  //     position: "top",
-  //     title: "登入成功！",
-  //     timer: 1000,
-  //     icon: "success",
-  //     showConfirmButton: false,
-  //   });
-  //   return;
-  // }
-  //
+    const data = await login({
+      account,
+      password,
+    });
+    const token = data.data.token;
 
-  //   // 登入失敗
-  //   if (data.status==='success') {
-  //       setErrorMessage(res.data.message);
-  //   }
-  // };
+    if (data.status === "success") {
+      localStorage.setItem("token", token);
+
+      // 登入成功訊息
+      Swal.fire({
+        position: "top",
+        title: "登入成功！",
+        timer: 1000,
+        icon: "success",
+        showConfirmButton: false,
+      });
+      return;
+    }
+
+    // 登入失敗
+    if (data.status === "error") {
+      setErrorMessage(data.message);
+    }
+  };
   return (
     <div className={styles.container}>
       <img className={styles.logo} src={IconLogo} alt="logo.svg" />
@@ -57,13 +56,10 @@ const LoginPage = () => {
           label={"帳號"}
           placeholder={"請輸入帳號"}
           value={account}
-          borderMode={clsx(
-            { [styles.inputButtomBorder]: !errorMessage },
-            {
-              [styles.accountInfoError]:
-                errorMessage === "[Your fail messages]",
-            }
-          )}
+          borderMode={clsx("", {
+            [styles.accountInfoError]:
+              errorMessage === "Error: Incorrect password!",
+          })}
           onChange={(accountInput) => {
             setErrorMessage("");
             setAccount(accountInput);
@@ -74,13 +70,10 @@ const LoginPage = () => {
           placeholder={"請輸入密碼"}
           type={"password"}
           value={password}
-          borderMode={clsx(
-            { [styles.inputButtomBorder]: !errorMessage },
-            {
-              [styles.passwordInfoError]:
-                errorMessage === "Error: Incorrect password!",
-            }
-          )}
+          borderMode={clsx("", {
+            [styles.passwordInfoError]:
+              errorMessage === "Error: Incorrect password!",
+          })}
           onChange={(passwordInput) => {
             setErrorMessage("");
             setPassword(passwordInput);
@@ -88,16 +81,12 @@ const LoginPage = () => {
         />
       </div>
       <div className={styles.loginButton}>
-        <AuthBtn text={"登入"} />
+        <AuthBtn text={"登入"} onClick={handleClick} />
       </div>
       <div className={styles.otherLink}>
-        <Link to="/signup">
-          <div className={styles.register}>註冊</div>
-        </Link>
+        <div className={styles.register}>註冊</div>
         <span className={styles.dot}>．</span>
-        <Link to="/admin">
-          <div className={styles.admin}>後台登入</div>
-        </Link>
+        <div className={styles.admin}>後台登入</div>
       </div>
     </div>
   );
