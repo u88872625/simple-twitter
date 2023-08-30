49498a079e98c9c0da36e206f74e5887fc351eb2
u88872625 <u88872625@gmail.com>
2023-08-30 16:03:14 +0800

    fix:delete LikeContext

--- src/App.js
+++ src/App.js
@@ -17,47 +17,38 @@ import AddTweet from "./components/AddTweet/AddTweet";
 
 import "./styles/App.module.scss";
 import { AuthProvider } from "./contexts/AuthContext";
-import {TweetIdContextProvider} from './contexts/TweetIdContext'
-import {LikeContexProvider} from './contexts/LikeContext'
-
-
+import { TweetIdContextProvider } from "./contexts/TweetIdContext";
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
         <AuthProvider>
-          <LikeContexProvider>
-            <TweetIdContextProvider>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/home" element={<MainPage />} />
-                <Route path="/:account" element={<UserPage />} />
-                <Route path="/settings" element={<SettingPage />} />
-                {/* <Route path="/settings/profile" element={<Modal />} />  */}
-                <Route path="/:username/replies" element={<TweetTabs />} />
-                <Route path="/:username/likes" element={<TweetTabs />} />
-                <Route
-                  path="/:username/followers"
-                  element={<UserFollowPage />}
-                />
-                <Route
-                  path="/:username/followering"
-                  element={<UserFollowPage />}
-                />
-                <Route path="/compose" element={<AddTweet />} />
-                {/* <Route path="/compose" element={<Modal />} /> */}
-                <Route path="/status/:id" element={<StatusPage />} />
-                <Route path="/admin" element={<AdminLoginPage />} />
-                <Route path="/admin/main" element={<AdminMainPage />} />
-                <Route path="/admin/users" element={<AdminUsersPage />} />
-                <Route path="*" element={<HomePage />} />
-              </Routes>
-            </TweetIdContextProvider>
-          </LikeContexProvider>
+          <TweetIdContextProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/home" element={<MainPage />} />
+              <Route path="/:account" element={<UserPage />} />
+              <Route path="/settings" element={<SettingPage />} />
+              {/* <Route path="/settings/profile" element={<Modal />} />  */}
+              <Route path="/:username/replies" element={<TweetTabs />} />
+              <Route path="/:username/likes" element={<TweetTabs />} />
+              <Route path="/:username/followers" element={<UserFollowPage />} />
+              <Route
+                path="/:username/followering"
+                element={<UserFollowPage />}
+              />
+              <Route path="/compose" element={<AddTweet />} />
+              {/* <Route path="/compose" element={<Modal />} /> */}
+              <Route path="/status/:id" element={<StatusPage />} />
+              <Route path="/admin" element={<AdminLoginPage />} />
+              <Route path="/admin/main" element={<AdminMainPage />} />
+              <Route path="/admin/users" element={<AdminUsersPage />} />
+              <Route path="*" element={<HomePage />} />
+            </Routes>
+          </TweetIdContextProvider>
         </AuthProvider>
       </BrowserRouter>
     </div>
