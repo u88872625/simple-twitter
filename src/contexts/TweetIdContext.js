64f65b0a267e85a69ed2a2d14bf29c59f4807bb7
u88872625 <u88872625@gmail.com>
2023-08-31 21:13:56 +0800

    fix:files fix

--- src/contexts/TweetIdContext.js
+++ src/contexts/TweetIdContext.js
@@ -15,33 +15,46 @@ export function TweetIdContextProvider({ children }) {
   const navigate = useNavigate();
   const location = useLocation();
 
+  // 儲存點擊的 tweetId
+  function handleSetTweetIdClick(tweetIdReceived) {
+    setTweetId(tweetIdReceived);
+    localStorage.setItem("tweetId", tweetIdReceived);
+  }
+
   // const { isDataUpdate, setIsDataUpdate } = useDataUpdate();
 
-  // useEffect(()=>{
-  //   if(tweetId) {
-  //     const fetchData = async()=>{
-  //       try{
-  //         const tweetRes = await getTopTweet(tweetId)
-  //         const repliesRes = await getTopTweetReplies(tweetId)
-  //         console.log('tweetidcontext:', tweetRes)
-  //         setTweetData(tweetRes)
-  //         setRepliesData(repliesRes)
-  //       } catch(error){
-  //         console.error(error)
+  // useEffect(() => {
+  //   if (tweetId) {
+  //     const fetchData = async () => {
+  //       try {
+  //         const tweetRes = await getTopTweet(tweetId);
+  //         const repliesRes = await getTopTweetReplies(tweetId);
+  //         console.log("tweetidcontext:", tweetRes);
+  //         setTweetData(tweetRes);
+  //         setRepliesData(repliesRes);
+  //       } catch (error) {
+  //         console.error(error);
   //       }
-  //     }
-  //     fetchData()
+  //     };
+  //     fetchData();
   //   }
-  // },[tweetId])
+  // }, [tweetId]);
 
   // 參數為被點擊的貼文id和點擊時的頁面
+
+  useEffect(() => {
+    const savedTweetId = localStorage.getItem("tweetId");
+    if (savedTweetId) {
+      setTweetId(savedTweetId);
+    }
+  }, []);
 
   const handleTweetClick = async (id, location) => {
     try {
       const tweetRes = await getTopTweet(id);
       const repliesRes = await getTopTweetReplies(id);
 
-      setTweetId(id);
+      // setTweetId(id);
       setTweetData(tweetRes);
       setRepliesData(repliesRes);
       navigate(`/status/${id}`, { state: { from: location.pathname } });
@@ -52,7 +65,14 @@ export function TweetIdContextProvider({ children }) {
 
   return (
     <TweetIdContext.Provider
-      value={{ tweetId, setTweetId, handleTweetClick, tweetData, repliesData }}
+      value={{
+        tweetId,
+        setTweetId,
+        handleTweetClick,
+        tweetData,
+        repliesData,
+        handleSetTweetIdClick,
+      }}
     >
       {children}
     </TweetIdContext.Provider>
