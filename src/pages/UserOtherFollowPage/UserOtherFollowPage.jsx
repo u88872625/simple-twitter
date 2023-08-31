2b4b2a6d8935a7cef98a4a70a53bf10578661412
Yunj0ng <chuccat@gmail.com>
2023-08-31 12:00:46 +0900

    feat:add OtherUserFollowPage

---
+++ src/pages/UserOtherFollowPage/UserOtherFollowPage.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import FontendLayout from "../../components/shared/layout/FontendLayout/FontendLayout";
+import FollowTabs from "../../components/FollowTabs/FollowTabs";
+import arrow from "../../assets/icons/back.svg";
+import styles from "./UserFollowPage.module.scss";
+import { getUserFollowers, getUserFollowings } from "../../api/tweets";
+import { getUserTweet } from "../../api/user";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+
+const UserFollowPage = () => {
+  const userId = localStorage.getItem("userId");
+	const otherUserId = localStorage.getItem("otherUserId");
+  const [userTweets, setUserTweets] = useState([]);
+  const [userFollowings, setUserFollowings] = useState([]);
+  const [userFollowers, setUserFollowers] = useState([]);
+  // 與popularList聯動重新渲染
+  const [rerender, setRerender] = useState(false);
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
+  // 返回上一頁
+  function handleUserPageClick() {
+    navigate("/:id");
+  }
+
+  useEffect(() => {
+    // 取得所有followers資料
+    const getUserFollowingsAsync = async () => {
+      try {
+        const followings = await getUserFollowings(otherUserId);
+
+        setUserFollowings(followings);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    //  取得所有followers資料
+    const getUserFollowersAsync = async () => {
+      try {
+        const followers = await getUserFollowers(otherUserId);
+
+        setUserFollowers(followers);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // 取得所有貼文
+    const getUserTweetAsync = async () => {
+      try {
+        const userTweets = await getUserTweet(otherUserId);
+        setUserTweets(userTweets);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (userId) {
+      getUserFollowingsAsync();
+      getUserFollowersAsync();
+      getUserTweetAsync();
+    } else {
+      navigate("/login");
+    }
+  }, [rerender, userId, navigate]);
+
+  return (
+    <div>
+      <FontendLayout rerender={rerender} setRerender={setRerender}>
+        <div className={styles.header}>
+          <img
+            className={styles.backArrow}
+            src={arrow}
+            onClick={handleUserPageClick}
+          ></img>
+          <div className={styles.userInfo}>
+            <h5 className={styles.name}>{otherUserId?.name}</h5>
+            <p className={styles.tweetCount}> {userTweets.length}推文</p>
+          </div>
+        </div>
+        <FollowTabs
+          followers={userFollowers}
+          followings={userFollowings}
+          rerender={rerender}
+          setRerender={setRerender}
+        />
+      </FontendLayout>
+    </div>
+  );
+};
+
+export default UserFollowPage;
