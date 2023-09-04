10c9b692c8f5fd37dda2c12b106fa3a465fa98b1
u88872625 <u88872625@gmail.com>
2023-09-05 01:25:55 +0800

    feat: popularList fixed

--- src/components/shared/layout/FontendLayout/FontendLayout.jsx
+++ src/components/shared/layout/FontendLayout/FontendLayout.jsx
@@ -1,7 +1,7 @@
 import styles from "./FontendLayout.module.scss";
 import FontendSideBar from "../../../SideBar/FontendSideBar.jsx";
 import PopularList from "../../../PopularList/PopularList";
-// import { userInfo } from "os";
+import { useEffect, useState } from "react";
 
 export default function FontendLayout({
   children,
@@ -9,13 +9,41 @@ export default function FontendLayout({
   setRerender,
   userInfo,
 }) {
+  const [rightContainerLeft, setRightContainerLeft] = useState(0);
+
+  useEffect(() => {
+    const updateRightContainerPosition = () => {
+      const mainContainer = document.querySelector(`.${styles.mainContainer}`);
+      const rightContainer = document.querySelector(
+        `.${styles.rightContainer}`
+      );
+      if (mainContainer && rightContainer) {
+        const rect = mainContainer.getBoundingClientRect();
+        setRightContainerLeft(rect.right + 24); // 24 是 .mainContainer 的 margin-right
+      }
+    };
+
+    // 初始設置
+    updateRightContainerPosition();
+
+    // 監聽 resize 事件
+    window.addEventListener("resize", updateRightContainerPosition);
+
+    // 清除事件監聽
+    return () => {
+      window.removeEventListener("resize", updateRightContainerPosition);
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
         <FontendSideBar userInfo={userInfo} />
       </div>
       <div className={styles.mainContainer}>{children}</div>
-      <div className={styles.rightContainer}>
+      <div
+        className={styles.rightContainer}
+        style={{ left: `${rightContainerLeft}px` }}
+      >
         <PopularList rerender={rerender} setRerender={setRerender} />
       </div>
     </div>
