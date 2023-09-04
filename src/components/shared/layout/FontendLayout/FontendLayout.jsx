81b246754a36dd1beeb94528f2b0be3fc3eff338
u88872625 <u88872625@gmail.com>
2023-09-05 02:47:55 +0800

    fix:popularList position

--- src/components/shared/layout/FontendLayout/FontendLayout.jsx
+++ src/components/shared/layout/FontendLayout/FontendLayout.jsx
@@ -1,7 +1,7 @@
 import styles from "./FontendLayout.module.scss";
 import FontendSideBar from "../../../SideBar/FontendSideBar.jsx";
 import PopularList from "../../../PopularList/PopularList";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function FontendLayout({
   children,
@@ -9,40 +9,62 @@ export default function FontendLayout({
   setRerender,
   userInfo,
 }) {
+  const mainContainerRef = useRef(null);
+  const rightContainerRef = useRef(null);
+  const [isMainAtBottom, setIsMainAtBottom] = useState(false);
   const [rightContainerLeft, setRightContainerLeft] = useState(0);
 
   useEffect(() => {
     const updateRightContainerPosition = () => {
-      const mainContainer = document.querySelector(`.${styles.mainContainer}`);
-      const rightContainer = document.querySelector(
-        `.${styles.rightContainer}`
-      );
-      if (mainContainer && rightContainer) {
-        const rect = mainContainer.getBoundingClientRect();
-        setRightContainerLeft(rect.right + 24); // 24 是 .mainContainer 的 margin-right
+      if (mainContainerRef.current) {
+        const rect = mainContainerRef.current.getBoundingClientRect();
+        setRightContainerLeft(rect.right + 24);
       }
     };
 
-    // 初始設置
-    updateRightContainerPosition();
+    const handleScroll = () => {
+      const main = mainContainerRef.current;
 
-    // 監聽 resize 事件
+      if (main.scrollTop + main.clientHeight >= main.scrollHeight) {
+        setIsMainAtBottom(true);
+      } else {
+        setIsMainAtBottom(false);
+      }
+      updateRightContainerPosition();
+    };
+
+    const main = mainContainerRef.current;
+    if (main) {
+      main.addEventListener("scroll", handleScroll);
+    }
+
+    updateRightContainerPosition();
     window.addEventListener("resize", updateRightContainerPosition);
 
-    // 清除事件監聽
     return () => {
+      if (main) {
+        main.removeEventListener("scroll", handleScroll);
+      }
       window.removeEventListener("resize", updateRightContainerPosition);
     };
   }, []);
+
+  const rightContainerStyle = isMainAtBottom
+    ? { position: "relative" }
+    : { position: "fixed", left: `${rightContainerLeft}px` };
+
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
         <FontendSideBar userInfo={userInfo} />
       </div>
-      <div className={styles.mainContainer}>{children}</div>
+      <div ref={mainContainerRef} className={styles.mainContainer}>
+        {children}
+      </div>
       <div
+        ref={rightContainerRef}
         className={styles.rightContainer}
-        style={{ left: `${rightContainerLeft}px` }}
+        style={rightContainerStyle}
       >
         <PopularList rerender={rerender} setRerender={setRerender} />
       </div>
